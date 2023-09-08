866efd138e52004eb7d8ec18312827f4a17aedad
Baf-03 <bilalahmedfarooqi03@gmail.com>
2023-09-09 01:40:50 +0500

    addingconditinal-Rendering

--- src/App.jsx
+++ src/App.jsx
@@ -7,76 +7,130 @@ import TextField from "@mui/material/TextField";
 import Buttons from "./Components/Buttons";
 
 function App() {
-  const [todoelements, settodoelements] = useState("");
-  const [uiShow, setuiShow] = useState([]);
+  let [inputtodo, setinputtodo] = useState("");
+  let [showui, setshowui] = useState([]);
+  let [uiinput, setuiInput] = useState("");
 
-  const Add_todo = () => {
-    uiShow.push(todoelements);
-    setuiShow([...uiShow]);
+  //--------------------------delete element from ui show-------------------
+
+  const del_element = (index) => {
+    showui.splice(index, 1);
+    setshowui([...showui]);
+  };
+  //--------------------------delete all element from ui show-------------------
+  const del_all = () => {
+    setshowui([]);
   };
 
-  const Delete_all = () => {
-    uiShow.splice(0, uiShow.length);
-    setuiShow([...uiShow]);
+  //-----------------------------------adding notes to ui-------------------------
+  const add_todo = () => {
+    let obj_todo = {
+      isEdit: false,
+      value: inputtodo,
+    };
+    showui.push(obj_todo);
+    setshowui([...showui]);
+    setinputtodo("");
   };
+  console.log(inputtodo);
+  console.log(showui);
 
-  const Edit_todo = (index) => {
-    console.log(index);
-    index = parseInt(index);
-    uiShow[index] = prompt("enter data");
-    setuiShow([...uiShow]);
-    console.log("edit todo is in action");
-    console.log(uiShow);
+  // ----------------------------------------------Editing todo element after its creation------------------------
+  const edit_todo = (index) => {
+
+    showui.forEach((element) => {
+      element.isEdit = false; //closing all previous opened edit by setting false and using ternary operator scroll down to see at line 84
+    });
+
+    showui[index].isEdit = true;//making it true  so that current element k liyae conditional rendering chal sakae 
+    setshowui([...showui]);
+    setuiInput(showui[index].value);
   };
-  const del_todo = (index) => {
-    uiShow.splice(index, 1);
-    setuiShow([...uiShow]);
+  //-----------------------------------after pressing edit button this ui will appear-----------------------------------------
+  const edit_todo_element = (index) => {
+    showui[index].isEdit = false;
+    showui[index].value = uiinput;
+    setshowui([...showui]);
+    uiinput[index] = showui[index].value;
+    setuiInput([...uiinput]);
   };
   return (
     <>
-      <section>
-        <Header />
+    {/* -----------------------------header------------------------------------------ */}
+      <Header />
+
+      {/* ----------------------------------------input field------------------------------------- */}
+      <section className="flex justify-center flex-col items-center mt-5 ">
+        <div className="w-[40%]">
+          <TextField
+            id="outlined-basic"
+            label="Add todo"
+            variant="outlined"
+            className="w-[100%]"
+            onChange={(e) => {
+              setinputtodo(e.target.value);
+            }}
+            value={inputtodo}
+          />
+        </div>
+
+      {/* ----------------------------------------buttons after input field------------------------------------- */}
+        <div className="flex gap-2 mt-4">
+          <Buttons value="Add" trigger={add_todo} />
+          <Buttons value="Delete" trigger={del_all} color="error" />
+        </div>
       </section>
 
-      <section className="mx-auto text-center mt-5 ">
-        <TextField
-          id="outlined-basic"
-          label="Enter Todo"
-          variant="outlined"
-          className="w-[80%] md:w-[80%] lg:w-[40%]"
-          onChange={(e) => settodoelements(e.target.value)}
-        />
-      </section>
-
-      <section className="flex justify-center m-3 gap-2 ">
-        {/* <Button onClick={handleClick}>Open simple snackbar</Button> */}
-
-        <Buttons value="Add todo" trigger={Add_todo} />
-
-        <Buttons value="Delete all" color="error" trigger={Delete_all} />
-      </section>
-
-      {uiShow.map((value, index) => {
+      {/* ---------------------------------------------todo display portion--------------------------------- */}
+      {showui.map((element, index) => {
         return (
-       
-            <section key={index} className=" w-[90%]  md:w-[70%] lg:w-[40%] flex flex-col justify-center mx-auto border border-solid rounded-lg border-black p-3 mt-[2%] ">
-              <div className="flex flex-row mt-2">
-                <div className="w-[60%] overflow-x-auto m-2">{value}</div>
-                <div className="flex gap-3">
-                  <Buttons value="Edit todo" trigger={() => Edit_todo(index)} />
-                  <Buttons
-                    value="Delete Todo"
-                    color="error"
-                    trigger={() => del_todo(index)}
-                  />
-                </div>
-              </div>
+          <section
+            className="flex justify-center flex-col items-center mt-5 "
+            key={index}
+          >
+            <section className=" flex items-center border border-gray-600 mt-5 w-[40%] p-3 rounded-lg">
+              {element.isEdit ? (
+                <>
+                  <div className="w-[80%] me-1">
+                    <TextField
+                      id="standard-basic"
+                      label=""
+                      variant="standard"
+                      className="w-[100%] "
+                      onChange={(e) => {
+                        setuiInput(e.target.value);
+                      }}
+                      value={uiinput}
+                      
+                    />
+                  </div>
+                  <div className="flex items-center gap-2 ">
+                    <Buttons
+                      value="Save"
+                      trigger={() => edit_todo_element(index)}
+                    />
+                    <Buttons value="Delete" color="error" />
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className="w-[80%]">{element.value}</div>
+                  <div className="flex items-center gap-2 ">
+                    <Buttons value="Edit" trigger={() => edit_todo(index)} />
+                    <Buttons
+                      value="Delete"
+                      trigger={() => {
+                        del_element(index);
+                      }}
+                      color="error"
+                    />
+                  </div>
+                </>
+              )}
             </section>
-       
+          </section>
         );
       })}
-
-      <h1>{todoelements}</h1>
     </>
   );
 }
